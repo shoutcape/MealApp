69152adb15748e396f55832a5b198274c65a420c
shoutcape <kautiainen.ville@gmail.com>
2024-03-24 20:51:32 +0200

    fixed formatting the code and added comments for clarity on main.js

--- main.js
+++ main.js
@@ -1,61 +1,62 @@
 let searchForm = document.querySelector('#searchBox');
 let foodResultsDiv = document.getElementById('foodResults');
 
-// prevent site from reloading if the form is submitted
+// on page load update the searchResults and categories
+window.onload = function () {
+    getCategories();
+    getAndShowSearchResults();
+};
+
+// prevent site from reloading if the form is submitted aka enter key is pressed
 searchForm.onsubmit = function (event) {
     event.preventDefault();
 };
 
-// on search input
+// eventlistener for the search input
 searchForm.oninput = function (event) {
     event.preventDefault();
     let searchTerm = searchForm['search'].value;
     // use session storage to save the search term to the browser session
     sessionStorage.setItem('searchTerm', searchTerm);
-    sessionStorage.removeItem('category')
-    getSearchResults();
+    sessionStorage.removeItem('category');
+    getAndShowSearchResults();
     // reset the category to default
-    categoryList.selectedIndex = 0
+    categoryList.selectedIndex = 0;
 };
 
-let categoryList = document.getElementById('categoryList')
+let categoryList = document.getElementById('categoryList');
 
+// eventlistener for the category dropdown
 categoryList.onchange = function (event) {
-    event.preventDefault()
-    let selectedCategory = event.target.value
-    sessionStorage.setItem('category', selectedCategory)
-    sessionStorage.removeItem('searchTerm')
-    getSearchResults()
-}
-
-window.onload = function () {
-    getCategories();
-    getSearchResults();
+    event.preventDefault();
+    let selectedCategory = event.target.value;
+    sessionStorage.setItem('category', selectedCategory);
+    sessionStorage.removeItem('searchTerm');
+    getAndShowSearchResults();
 };
 
-function getSearchResults() {
+function getAndShowSearchResults() {
     // get the searchterm from sessionStorage
     let searchTerm = sessionStorage.getItem('searchTerm');
-    let category = sessionStorage.getItem('category')
+    let category = sessionStorage.getItem('category');
     let api = 'https://www.themealdb.com/api/json/v1/1/';
 
     // default searchmode
-    let searchMode = 'search.php?s='
+    let searchMode = 'search.php?s=';
 
     searchForm['search'].value = searchTerm;
     // check if category is selected
     if (category && category !== 'none') {
-        searchTerm = category
+        searchTerm = category;
         // category searchmode
-        searchMode = 'filter.php?c='
+        searchMode = 'filter.php?c=';
     }
-    
+
     // if no searchTerm or category don't continue
     if (searchTerm == '' && (category == 'none' || !category)) {
         foodResultsDiv.innerHTML = '';
         return;
     }
-
 
     // Fetch the meals from the api with the searchTerm
     fetch(`${api}${searchMode}${searchTerm}`)
@@ -107,19 +108,19 @@ function getSearchResults() {
 function getCategories() {
     let api = 'https://www.themealdb.com/api/json/v1/1/';
     fetch(`${api}categories.php`)
-    .then((response) => response.json())
-    .then((data) => {
-        let categories = data.categories
-        for (let item of categories) {
-            let newOption = document.createElement('option')
-            newOption.value = item.strCategory
-            newOption.innerHTML = item.strCategory
-            categoryList.appendChild(newOption)
-        }
-        // if sessionstorage has category show it as selected on the dropdown
-        let category = sessionStorage.getItem('category')
-        if (category) {
-            categoryList.value = category
-        }
-    })
+        .then((response) => response.json())
+        .then((data) => {
+            let categories = data.categories;
+            for (let item of categories) {
+                let newOption = document.createElement('option');
+                newOption.value = item.strCategory;
+                newOption.innerHTML = item.strCategory;
+                categoryList.appendChild(newOption);
+            }
+            // if sessionstorage has category show it as selected on the dropdown
+            let category = sessionStorage.getItem('category');
+            if (category) {
+                categoryList.value = category;
+            }
+        });
 }
